cf32a6267e71774bc60cdc2e6e65e5812af54009
Никита Стручков <nikitastruckov@Noutbuk-Nikita.local>
2024-01-31 21:29:26 +0300

    auth

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,14 @@ const userRouter = require('express').Router();
 const auth = require('../middlewares/auth');
 
 const { getUserInfo, updateUser } = require('../controllers/user');
+// Проверка авторизации
+
+userRouter.use(auth);
 
 // возвращает информацию о пользователе (email и имя)
-userRouter.get('/users/me', auth, getUserInfo);
+userRouter.get('/users/me', getUserInfo);
 
 // обновляет информацию о пользователе (email и имя)
-userRouter.patch('/users/me', auth, updateUser);
+userRouter.patch('/users/me', updateUser);
 
 module.exports = userRouter;
